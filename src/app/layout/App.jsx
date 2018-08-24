3740e0f5f4cffde7335dd68f376773bcc78e0923
Bassem <bassem@coradine.com>
2018-08-24 07:43:20 +0200

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Revent</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
